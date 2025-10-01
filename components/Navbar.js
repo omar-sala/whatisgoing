97b1dd41e1c04a0dc419f8bf8201b785b7cf1756
omar salama <omarsalamaali870@gmail.com>
2025-10-01 14:31:00 +0300

    update Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
@@ -7,58 +9,26 @@ import {
   faUser,
   faBars,
 } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const closeMenu = () => setMenuOpen(false)
+
   return (
-    <nav className="bg-blue-800 text-white shadow-md">
+    <nav className="bg-blue-800 text-white shadow-md relative">
       <div className="container mx-auto px-4 py-3 flex justify-between items-center">
         {/* الشعار */}
         <h1 className="text-xl font-bold">🌍 NewsInsight</h1>
 
-        {/* input type checkbox للتحكم في القوائم على الموبايل */}
-        <div className="md:hidden">
-          <input type="checkbox" id="menu-toggle" className="hidden peer" />
-          <label htmlFor="menu-toggle" className="text-2xl cursor-pointer">
-            <FontAwesomeIcon icon={faBars} />
-          </label>
-
-          {/* القوائم تظهر عند checked */}
-          <ul className="peer-checked:block hidden absolute top-full left-0 w-full bg-blue-800 flex flex-col">
-            <li>
-              <Link href="/" className="block px-4 py-2 hover:text-yellow-300">
-                <FontAwesomeIcon icon={faHouse} className="mr-2" />
-                الرئيسية
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/dashboard"
-                className="block px-4 py-2 hover:text-yellow-300"
-              >
-                <FontAwesomeIcon icon={faChartColumn} className="mr-2" />
-                لوحة التحكم
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/dashboard/top-entities"
-                className="block px-4 py-2 hover:text-yellow-300"
-              >
-                <FontAwesomeIcon icon={faLink} className="mr-2" />
-                الكيانات الأعلى
-              </Link>
-            </li>
-            <li>
-              <Link
-                href="/about"
-                className="block px-4 py-2 hover:text-yellow-300"
-              >
-                <FontAwesomeIcon icon={faUser} className="mr-2" />
-                عن المنصة
-              </Link>
-            </li>
-          </ul>
-        </div>
+        {/* زر الموبايل */}
+        <button
+          className="text-2xl cursor-pointer md:hidden"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </button>
 
         {/* الروابط للـ desktop */}
         <ul className="hidden md:flex md:gap-6 md:items-center">
@@ -97,6 +67,52 @@ export default function Navbar() {
           </li>
         </ul>
       </div>
+
+      {/* المنيو للموبايل */}
+      {menuOpen && (
+        <ul className="absolute top-full left-0 w-full bg-blue-800 flex flex-col text-center shadow-md md:hidden z-50">
+          <li>
+            <Link
+              href="/"
+              className="block px-4 py-3 hover:text-yellow-300"
+              onClick={closeMenu}
+            >
+              <FontAwesomeIcon icon={faHouse} className="mr-2" />
+              الرئيسية
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dashboard"
+              className="block px-4 py-3 hover:text-yellow-300"
+              onClick={closeMenu}
+            >
+              <FontAwesomeIcon icon={faChartColumn} className="mr-2" />
+              لوحة التحكم
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/dashboard/top-entities"
+              className="block px-4 py-3 hover:text-yellow-300"
+              onClick={closeMenu}
+            >
+              <FontAwesomeIcon icon={faLink} className="mr-2" />
+              الكيانات الأعلى
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/about"
+              className="block px-4 py-3 hover:text-yellow-300"
+              onClick={closeMenu}
+            >
+              <FontAwesomeIcon icon={faUser} className="mr-2" />
+              عن المنصة
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   )
 }
